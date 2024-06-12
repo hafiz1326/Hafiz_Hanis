ae876a39995bb2c74981ab1f583ba5c8c64eb713
hanisnabila1 <hanisnabila2017@gmail.com>
2024-06-12 19:33:31 +0700

    update

--- modul/suplier/index.php
+++ modul/suplier/index.php
@@ -62,81 +62,79 @@ primary">Simpan</button>
 </tr>
 </thead>
 <tbody>
+<!-- Data Supplier -->
+<?php
+$query = "SELECT * from tbl_supplier";
+$exec = mysqli_query($koneksi, $query);
+$no = 1;
+while($data = mysqli_fetch_array($exec)) {
+?>
 <tr>
-<td>1</td>
-<td>PT. ABC</td>
-<td>Jl. ABC</td>
-<td>08123456789</td>
-<td>oK5Zt@example.com</td>
+<td><?= $no++ ?></td>
+<td><?= $data['nama_supplier'] ?></td>
+<td><?= $data['alamat')] ?></td>
+<td><?= $data['telp'] ?></td>
+<td><?= $data['email'] ?></td>
 <td>
-<a href="#editSuplier" class="text-decoration-none" data-bs-
-toggle="modal">
+<a href="#editSupplier<?= $data['id'] ?>" class="text-
+decoration-none" data-bs-toggle="modal">
 <i class="bi bi-pencil-square text-success"></i>
 </a>
-<a href="" class="text-decoration-none">
+<a href="modul/suplier/aksi_suplier.php?act=delete&id=<?=
+$data['id'] ?>" class="text-decoration-none">
 <i class="bi bi-trash text-danger"></i>
 </a>
 </td>
-<!-- Modal -->
-<div class="modal fade" id="editSuplier" tabindex="-1" aria-
-labelledby="exampleModalLabel" aria-hidden="true">
+</tr>
+<!-- Modal Edit Supplier -->
+<div class="modal fade" id="editSupplier<?= $data['id'] ?>"
+tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<form action="modul/suplier/aksi_suplier.php?act=update&id=<?=
+$data['id'] ?>" method="post">
 <div class="modal-dialog">
 <div class="modal-content">
 <div class="modal-header">
-Suplier</h5>
-<h5 class="modal-title" id="exampleModalLabel">Edit
+<h5 class="modal-title" id="exampleModalLabel">EditSupplier</h5>
+
 <button type="button" class="btn-close" data-bs-
 dismiss="modal" aria-label="Close"></button>
 </div>
 <div class="modal-body">
 <div class="mb-3">
-<label class="form-label" for="nama_suplier">Nama
-suplier</label>
+<label class="form-label" for="nama_supplier">Nama
+Supplier</label>
 <input type="text" class="form-control"
-name="nama_suplier" value="PT. ABC">
+name="nama_supplier" value="<?= $data['nama_supplier'] ?>">
 </div>
 <div class="mb-3">
 <label class="form-label" for="alamat">Alamat</label>
-value="Jl. ABC">
 <input type="text" class="form-control" name="alamat"
+value="<?= $data['alamat'] ?>">
 </div>
 <div class="mb-3">
 <label class="form-label" for="telp">Telp</label>
 <input type="text" class="form-control" name="telp"
-value="08123456789">
+lue="<?= $data['telp'] ?>">
 </div>
 <div class="mb-3">
 <label class="form-label" for="email">Email</label>
 <input type="text" class="form-control" name="email"
-value="oK5Zt@example.com">
+value="<?= $data['email'] ?>">
 </div>
 </div>
 <div class="modal-footer">
 <button type="button" class="btn btn-secondary" data-bs-
 dismiss="modal">Close</button>
-<button type="button" class="btn btn-primary">Save
+<button type="submit" class="btn btn-primary">Save
 changes</button>
 </div>
 </div>
 </div>
+</form>
 </div>
-</tr>
-<tr>
-<td>2</td>
-<td>PT. XYZ</td>
-<td>J1. XYZ</td>
-<td>08123456789</td>
-<td>oK5Zt@example.com</td>
-<td>
-<a href="#editSuplier" class="text-decoration-none"
-data-bs-toggle="modal">
-<i class="bi bi-pencil-square text-success"></i>
-</a>
-<a href="" class="text-decoration-none">
-<i class="bi bi-trash text-danger"></i>
-</a>
-</td>
-</tr>
+<?php
+}
+?>
 </tbody>
 </table>
 </div>
